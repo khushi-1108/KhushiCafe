e44f490d9ab8dcfb71cbec65d23ddbf25e0374d3
khushi-1108 <kjariwala707@gmail.com>
2023-12-07 04:19:52 -0500

    second commit

--- app/src/main/java/com/hfad/khushicafe/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/hfad/khushicafe/ui/notifications/NotificationsFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.hfad.khushicafe.R
 import com.hfad.khushicafe.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,16 +18,20 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
+        // Inflate the layout for this fragment using the binding
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        // Set up of event details here using the generated binding references
+        binding.ChristmasText.text = "Christmas Event"
+        binding.NewYearText.text = "New Year Event"
+        binding.ValentineText.text = "Valentine's Day Event"
+
+        //images into ImageViews
+        binding.ChristmasImage.setImageResource(R.drawable.christmas)
+        binding.NewYearImage.setImageResource(R.drawable.newyear)
+        binding.ValentineImage.setImageResource(R.drawable.valentine)
+
         return root
     }
 
